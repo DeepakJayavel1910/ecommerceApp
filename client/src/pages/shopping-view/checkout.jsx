53f3cb128e521b1141e9abf2a7ae8271b9a81d53
Deepak <jayaveldeepak19@gmail.com>
2024-11-14 23:55:03 +0530

    Deploy test 2

--- client/src/pages/shopping-view/checkout.jsx
+++ client/src/pages/shopping-view/checkout.jsx
@@ -3,8 +3,8 @@ import { ShieldCheck } from "lucide-react"
 const ShoppingCheckout = () => {
   return (
     <div className="flex flex-col justify-center items-center h-screen w-full">
-      <h1 className="text-[40px]">Your Order has been placed successfully</h1>
-      <ShieldCheck className="size-32 mt-10 mb-20" color="#45d142" />
+      <h1 className="text-2xl md:text-[40px] text-center">Your Order has been placed successfully</h1>
+      <ShieldCheck className="size-24 lg:size-32 mt-10 mb-20" color="#45d142" />
     </div>
   )
 }
